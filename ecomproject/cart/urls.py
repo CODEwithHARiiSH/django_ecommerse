00fee30e22565c004f284c6f5dd2bc924d683801
Harish Thampy <103177383+mrrideholic@users.noreply.github.com>
2023-06-07 17:34:21 +0530

    yes

---
+++ ecomproject/cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name='cart'
+urlpatterns=[
+    path('add/<int:product_id>/',views.add_cart,name='add_cart'),
+    path('delete_cart_item/<int:product_id>/',views.delete_cart_item,name='delete_cart_item'),
+    path('ckeckout/',views.checkout,name='checkout'),
+    path('',views.cart_detail,name='cart_detail'),
+
+]
